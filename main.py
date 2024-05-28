4a6d6828de91960f6fa522d0fdcaa50d2bb627f6
Oliver Ruben Albertini <oliverruben@gmail.com>
2024-05-27 17:15:06 -0700

    Refactor and try to add pyre-strict
    Also adds pyre/watchman files and gitignore.

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
+# pyre-strict
 
 import json
 import os
-import re
 import subprocess
 import sys
-import time
 from dataclasses import dataclass
-from datetime import timezone, datetime, timedelta
-from typing import List, Optional, Union
+from datetime import datetime, timezone
+from enum import Enum, StrEnum
+from typing import List, Mapping, Optional, Union
+
+
+class Format(StrEnum):
+    SANS_TIMEZONE = "%Y-%m-%d %H:%M:%S"
+    WITH_TIMEZONE = "%Y-%m-%d %H:%M:%S%z"
+    TIMESTAMP = "%s"
+
 
 DOCS = "https://www.gnu.org/software/coreutils/manual/html_node/Date-input-formats.html"
-FORMATS = [
-    "%Y-%m-%d %H:%M:%S",
-    "%Y-%m-%d %H:%M:%S%z",
-]
+DEFAULT_FORMAT: Format = Format.SANS_TIMEZONE
+
+
+class Source(Enum):
+    CURRENT = 1
+    CLIPBOARD = 2
+    ARGUMENT = 3
 
 
 @dataclass(frozen=True)
@@ -25,13 +35,29 @@ class Item:
 
 
 @dataclass(frozen=True)
+class DatetimeItem:
+    dt: datetime
+    source: Source
+    format_: Format
+
+    @property
+    def dict(self) -> Mapping[str, Union[str, Mapping[str, "DatetimeItem"]]]:
+        cmd = DatetimeItem(self.dt, self.source, Format.TIMESTAMP)
+        ctrl = DatetimeItem(self.dt, self.source, Format.WITH_TIMEZONE)
+        mods = {}
+        if self.format_ == DEFAULT_FORMAT:
+            mods = {"cmd": cmd, "ctrl": ctrl}
+        return {
+            "title": self.dt.strftime(Format.WITH_TIMEZONE.value),
+            "subtitle": f"Copy {self.source.name.title()} Time {self.format_.name.replace('_', ' ').title()} To Clipboard ({self.dt.tzinfo})",
+            "arg": self.dt.strftime(self.format_.value),
+            "mods": {k: v.dict for k, v in mods.items()},
+        }
+
+
+@dataclass(frozen=True)
 class Feedback:
-    items: List[Item]
-
-
-class CustomEncoder(json.JSONEncoder):
-    def default(self, obj):
-        return obj.__dict__
+    items: List[Union[DatetimeItem, Item]]
 
 
 def atoi(s: Union[int, str, float]) -> Optional[int]:
@@ -39,21 +65,6 @@ def atoi(s: Union[int, str, float]) -> Optional[int]:
         return int(s)
     except ValueError:
         return None
-
-
-def generate_items(t: datetime, desc: str):
-    ts = t.timestamp()
-    utc = datetime.fromtimestamp(ts, timezone.utc)
-    items = [Item(str(int(ts)), f"{desc} Timestamp", str(int(ts)))] + [
-        Item(t_.strftime(layout), f"{desc} ({t_.tzinfo})", t_.strftime(layout))
-        for t_ in [t, utc]
-        for layout in FORMATS
-    ]
-    return items
-
-
-def generate_feedback(items: List[Item]):
-    return CustomEncoder().encode(Feedback(items))
 
 
 def read_from_clipboard() -> str:
@@ -87,19 +98,27 @@ def main():
     try:
         items = list()
         arg = sys.argv[1].strip().lower() if len(sys.argv) > 1 else ""
-        if not arg:
-            now = datetime.now().astimezone()
-            cb = read_from_clipboard()
-            if ts := (atoi(cb) or gdate(cb) or None):
-                t = datetime.fromtimestamp(ts).astimezone()
-                items.extend(generate_items(t, f"From Clipboard: {cb}"))
-            items.extend(generate_items(now, "Current Time"))
+        if arg:
+            ts = atoi(arg) or gdate(arg)
+            source = Source.ARGUMENT
         else:
-            if ts := (gdate(arg) or gdate(f"@{arg}")):
-                t = datetime.fromtimestamp(ts).astimezone()
-                items.extend(generate_items(t, "From Arg"))
+            cb = read_from_clipboard()
+            ts = atoi(cb) or gdate(cb)
+            source = Source.CLIPBOARD
+        datetimes = []
+        if ts:
+            datetimes.append((datetime.fromtimestamp(ts).astimezone(), source))
+        datetimes.append((datetime.now().astimezone(), Source.CURRENT))
+        for t, source in datetimes:
+            items.append(DatetimeItem(t, source, DEFAULT_FORMAT))
+            utc = datetime.fromtimestamp(t.timestamp(), timezone.utc)
+            items.append(DatetimeItem(utc, source, DEFAULT_FORMAT))
         items.append(Item(DOCS, "Visit `gdate` docs", DOCS))
-        print(generate_feedback(items))
+        print(
+            json.dumps(
+                Feedback(items), default=lambda x: getattr(x, "dict", x.__dict__)
+            )
+        )
     except IndexError as e:
         print(f"Invalid input: {e}")
         exit(1)
